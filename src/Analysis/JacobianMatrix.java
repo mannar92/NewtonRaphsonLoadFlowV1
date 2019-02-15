1a320b7b7914653f5325b8bd6f0307f64fdff0d8
Alexandros Mannari <alex.mannaris@gmail.com>
2019-02-15 15:53:53 +0000

    Jacobian matrix class constructor jacobian matrix checked and verified.

--- src/Analysis/JacobianMatrix.java
+++ src/Analysis/JacobianMatrix.java
@@ -20,7 +20,7 @@ public class JacobianMatrix {
                           Branch[] branch, Generation[] generations,
                           PowerFlowEquations[] powerFlowEquations) {
 
-        jac = new double[bus.length*2][bus.length*2]
+        jac = new double[bus.length*2][bus.length*2];
 
         for (int k=0; k<bus.length; k++){
             for (int i=0; i<bus.length; i++){
@@ -30,49 +30,30 @@ public class JacobianMatrix {
                 gki = admittanceMatrix.getConductance()[k][i];
                 bki = admittanceMatrix.getSusceptance()[k][i];
                 if (k!=i){
-                    jac[k][i] += vk*vi*(gki*Math.sin(angleki)-bki*Math.cos(angleki));
-                    jac[k][i+bus.length] += vk*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                    jac[k+bus.length][i] += vk*vi*(-gki*Math.cos(angleki)-bki*Math.sin(angleki));
-                    jac[k+bus.length][i+bus.length] += vk*(gki*Math.sin(angleki)-bki*Math.cos(angleki));
+                    jac[k][i] += vk*vi*(gki*Math.sin(angleki)-bki*Math.cos(angleki));               //  partial derivative Pk with respect to angle at bus i.
+                    jac[k][i+bus.length] += vk*(gki*Math.cos(angleki)+bki*Math.sin(angleki));       //  partial derivative of Pk with respect to voltage at bus i.
+                    jac[k+bus.length][i] += vk*vi*(-gki*Math.cos(angleki)-bki*Math.sin(angleki));   //  partial derivative of Qk with respect to angle at bus i.
+                    jac[k+bus.length][i+bus.length] += vk*(gki*Math.sin(angleki)                    //  partial derivative of Qk with respect to voltage at bus i.
+                            -bki*Math.cos(angleki));
                 }
-                else {
+                else { // k == i
                     for (int n = 0; n<bus.length; n++){
                         vi = bus[n].getVoltageMagnitude();
                         angleki = bus[k].getVoltageAngle()-bus[n].getVoltageAngle();
                         gki = admittanceMatrix.getConductance()[k][n];
                         bki = admittanceMatrix.getSusceptance()[k][n];
-                        jac[k][k] += vk*vi*(-gki*Math.sin(angleki)+bki*Math.cos(angleki));
-                        jac[k][k+bus.length] += vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                        if (k!=n){
-                            jac[k+bus.length][k] += vk*vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                            jac[k+bus.length][k+bus.length] += vi*(gki*Math.sin(angleki)+bki*Math.cos(angleki));
-                        }
+                        jac[k][i] += vk*vi*(-gki*Math.sin(angleki)+bki*Math.cos(angleki));          // partial derivative Pk with respect to angle k
+                        jac[k][i+bus.length] += vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));   //  partial derivative Pk with respect to voltage k
+                        jac[k+bus.length][i] += vk*vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));//  partial derivative Qk with respect to angle at bus k
+                        jac[k+bus.length][i+bus.length] += vi*(gki*Math.sin(angleki)
+                                -bki*Math.cos(angleki));                                            //  partial derivative Qk with respect to voltage at bus k
                     }
+                    jac[k][i] += -bki*vk*vk;
+                    jac[k][i+bus.length] += gki*vk*vk;
+                    jac[k][i+bus.length] += -gki*vk*vk;
+                    jac[k+bus.length][i+bus.length] += -bki*vk*vk;
                 }
             }
-
-            for (int j=0; j<branch.length; j++){
-                i = ((int)branch[j].getToBusID()-1);
-                vk = bus[k].getVoltageMagnitude();
-                vi = bus[i].getVoltageMagnitude();
-                angleki = bus[k].getVoltageAngle()-bus[i].getVoltageAngle();
-                gki = admittanceMatrix.getConductance()[k][i];
-                bki = admittanceMatrix.getSusceptance()[k][i];
-                if (k!=i){
-                    jac[k][i] += vk*vi*(gki*Math.sin(angleki)-bki*Math.cos(angleki));
-                    jac[k][i+bus.length] += vk*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                    jac[k+bus.length][i] += vk*vi*(-gki*Math.cos(angleki)-bki*Math.sin(angleki));
-                    jac[k+bus.length][i+bus.length] += vk*(gki*Math.sin(angleki)-bki*Math.cos(angleki));
-                }
-                else {
-                    jac[k][i] += vk*vi*(-gki*Math.sin(angleki)+bki*Math.cos(angleki));
-                    jac[k][i+bus.length] += vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                    jac[k+bus.length][i] += vk*vi*(gki*Math.cos(angleki)+bki*Math.sin(angleki));
-                    jac[k+bus.length][i+bus.length] += vi*(gki*Math.sin(angleki)+bki*Math.cos(angleki));
-                }
-
-            }
-
         }
     }
 }
